e9aad7aa64c69223065abd3addb46d6a460ba116
Fineas Gavre <fineasgavre@gmail.com>
2020-09-16 15:45:36 +0300

    Initial Commit

---
+++ LocationizerPlus/LocationizerPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationizerPlusApp.swift
+//  LocationizerPlus
+//
+//  Created by Fineas Gavre on 16.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct LocationizerPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
